66e78d7ac696c4c03d34a75362caf58cc8990ac7
Unknown <deepti.dodda@gmail.com>
2018-11-08 16:42:08 -0500

    journey controller

---
+++ journeytransformer/src/main/java/net/atpco/hack/journeytransformer/client/TransformerController.java
@@ -0,0 +1,20 @@
+package net.atpco.hack.journeytransformer.client;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/batch")
+public class TransformerController {
+
+	@RequestMapping("/ping")
+	public String ping() {
+		return "PONG";
+	}
+
+
+}
