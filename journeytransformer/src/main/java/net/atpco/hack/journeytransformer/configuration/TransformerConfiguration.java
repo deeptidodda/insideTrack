4784216ad5b08c360914edfae7b074811f12184b
Unknown <deepti.dodda@gmail.com>
2018-11-08 16:56:18 -0500

    journey client

--- journeytransformer/src/main/java/net/atpco/hack/journeytransformer/configuration/TransformerConfiguration.java
+++ journeytransformer/src/main/java/net/atpco/hack/journeytransformer/configuration/TransformerConfiguration.java
@@ -3,6 +3,7 @@ package net.atpco.hack.journeytransformer.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -12,6 +13,11 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 
 @Configuration
 public class TransformerConfiguration {
+	
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 	
 	@Bean
 	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
