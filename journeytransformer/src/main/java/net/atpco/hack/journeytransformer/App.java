f6719fc73950c3e0658d74c91078077ac68126da
Unknown <deepti.dodda@gmail.com>
2018-11-08 16:02:59 -0500

    spring boot

--- journeytransformer/src/main/java/net/atpco/hack/journeytransformer/App.java
+++ journeytransformer/src/main/java/net/atpco/hack/journeytransformer/App.java
@@ -1,21 +1,20 @@
 package net.atpco.hack.journeytransformer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.admin.SpringApplicationAdminJmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.integration.IntegrationAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 
-@RestController
-@EnableAutoConfiguration
+@SpringBootApplication(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class, EmbeddedMongoAutoConfiguration.class,
+		JmxAutoConfiguration.class, SpringApplicationAdminJmxAutoConfiguration.class, 
+		IntegrationAutoConfiguration.class})
 public class App {
 
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
-	}
-
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
-
 }
