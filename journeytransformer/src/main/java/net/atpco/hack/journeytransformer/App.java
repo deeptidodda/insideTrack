4146416d22c7dde27ba4344ca526a72569919e63
Unknown <deepti.dodda@gmail.com>
2018-11-08 16:00:23 -0500

    spring boot

--- journeytransformer/src/main/java/net/atpco/hack/journeytransformer/App.java
+++ journeytransformer/src/main/java/net/atpco/hack/journeytransformer/App.java
@@ -1,12 +1,21 @@
 package net.atpco.hack.journeytransformer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication()
-public class App 
-{
-	public static void main(String[] args) {
+@RestController
+@EnableAutoConfiguration
+public class App {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(App.class, args);
 	}
+
 }
